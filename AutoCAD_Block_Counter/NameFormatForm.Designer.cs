6038d07203034244663c617bc41634f8ef435345
Mickey0207 <157815247+Mickey0207@users.noreply.github.com>
2025-06-28 16:18:16 +0800

    修改使用者介面大小

--- AutoCAD_Block_Counter/NameFormatForm.Designer.cs
+++ AutoCAD_Block_Counter/NameFormatForm.Designer.cs
@@ -49,7 +49,7 @@ namespace AutoCAD_Block_Counter
             // lblHint
             // 
             this.lblHint.AutoSize = true;
-            this.lblHint.Location = new System.Drawing.Point(12, 9);
+            this.lblHint.Location = new System.Drawing.Point(20, 20);
             this.lblHint.Name = "lblHint";
             this.lblHint.Size = new System.Drawing.Size(320, 15);
             this.lblHint.TabIndex = 0;
@@ -58,7 +58,7 @@ namespace AutoCAD_Block_Counter
             // lblFileName
             // 
             this.lblFileName.AutoSize = true;
-            this.lblFileName.Location = new System.Drawing.Point(12, 34);
+            this.lblFileName.Location = new System.Drawing.Point(20, 50);
             this.lblFileName.Name = "lblFileName";
             this.lblFileName.Size = new System.Drawing.Size(92, 15);
             this.lblFileName.TabIndex = 1;
@@ -66,15 +66,15 @@ namespace AutoCAD_Block_Counter
             // 
             // flowSegments
             // 
-            this.flowSegments.Location = new System.Drawing.Point(12, 60);
+            this.flowSegments.Location = new System.Drawing.Point(20, 80);
             this.flowSegments.Name = "flowSegments";
-            this.flowSegments.Size = new System.Drawing.Size(360, 60);
+            this.flowSegments.Size = new System.Drawing.Size(540, 40);
             this.flowSegments.TabIndex = 2;
             // 
             // lblPreview
             // 
             this.lblPreview.AutoSize = true;
-            this.lblPreview.Location = new System.Drawing.Point(12, 130);
+            this.lblPreview.Location = new System.Drawing.Point(20, 130);
             this.lblPreview.Name = "lblPreview";
             this.lblPreview.Size = new System.Drawing.Size(68, 15);
             this.lblPreview.TabIndex = 3;
@@ -82,16 +82,16 @@ namespace AutoCAD_Block_Counter
             // 
             // txtPreview
             // 
-            this.txtPreview.Location = new System.Drawing.Point(86, 127);
+            this.txtPreview.Location = new System.Drawing.Point(100, 127);
             this.txtPreview.Name = "txtPreview";
             this.txtPreview.ReadOnly = true;
-            this.txtPreview.Size = new System.Drawing.Size(200, 23);
+            this.txtPreview.Size = new System.Drawing.Size(460, 23);
             this.txtPreview.TabIndex = 4;
             // 
             // lblExcelName
             // 
             this.lblExcelName.AutoSize = true;
-            this.lblExcelName.Location = new System.Drawing.Point(12, 165);
+            this.lblExcelName.Location = new System.Drawing.Point(20, 170);
             this.lblExcelName.Name = "lblExcelName";
             this.lblExcelName.Size = new System.Drawing.Size(97, 15);
             this.lblExcelName.TabIndex = 7;
@@ -99,15 +99,15 @@ namespace AutoCAD_Block_Counter
             // 
             // txtExcelName
             // 
-            this.txtExcelName.Location = new System.Drawing.Point(110, 162);
+            this.txtExcelName.Location = new System.Drawing.Point(120, 167);
             this.txtExcelName.Name = "txtExcelName";
-            this.txtExcelName.Size = new System.Drawing.Size(176, 23);
+            this.txtExcelName.Size = new System.Drawing.Size(440, 23);
             this.txtExcelName.TabIndex = 8;
             // 
             // chkShowLayer
             // 
             this.chkShowLayer.AutoSize = true;
-            this.chkShowLayer.Location = new System.Drawing.Point(12, 180);
+            this.chkShowLayer.Location = new System.Drawing.Point(20, 205);
             this.chkShowLayer.Name = "chkShowLayer";
             this.chkShowLayer.Size = new System.Drawing.Size(180, 19);
             this.chkShowLayer.TabIndex = 12;
@@ -117,17 +117,17 @@ namespace AutoCAD_Block_Counter
             // dgvBlocks
             // 
             this.dgvBlocks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvBlocks.Location = new System.Drawing.Point(12, 210);
+            this.dgvBlocks.Location = new System.Drawing.Point(20, 240);
             this.dgvBlocks.Name = "dgvBlocks";
             this.dgvBlocks.RowTemplate.Height = 25;
-            this.dgvBlocks.Size = new System.Drawing.Size(360, 200);
+            this.dgvBlocks.Size = new System.Drawing.Size(540, 260);
             this.dgvBlocks.TabIndex = 9;
             // 
             // btnAddBlock
             // 
-            this.btnAddBlock.Location = new System.Drawing.Point(12, 420);
+            this.btnAddBlock.Location = new System.Drawing.Point(20, 520);
             this.btnAddBlock.Name = "btnAddBlock";
-            this.btnAddBlock.Size = new System.Drawing.Size(100, 30);
+            this.btnAddBlock.Size = new System.Drawing.Size(120, 35);
             this.btnAddBlock.TabIndex = 10;
             this.btnAddBlock.Text = "新增圖塊列";
             this.btnAddBlock.UseVisualStyleBackColor = true;
@@ -135,9 +135,9 @@ namespace AutoCAD_Block_Counter
             // 
             // btnImportBlockMap
             // 
-            this.btnImportBlockMap.Location = new System.Drawing.Point(120, 420);
+            this.btnImportBlockMap.Location = new System.Drawing.Point(160, 520);
             this.btnImportBlockMap.Name = "btnImportBlockMap";
-            this.btnImportBlockMap.Size = new System.Drawing.Size(100, 30);
+            this.btnImportBlockMap.Size = new System.Drawing.Size(120, 35);
             this.btnImportBlockMap.TabIndex = 11;
             this.btnImportBlockMap.Text = "匯入圖塊對應";
             this.btnImportBlockMap.UseVisualStyleBackColor = true;
@@ -145,9 +145,9 @@ namespace AutoCAD_Block_Counter
             // 
             // btnOK
             // 
-            this.btnOK.Location = new System.Drawing.Point(230, 420);
+            this.btnOK.Location = new System.Drawing.Point(320, 520);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(65, 30);
+            this.btnOK.Size = new System.Drawing.Size(100, 35);
             this.btnOK.TabIndex = 5;
             this.btnOK.Text = "確定";
             this.btnOK.UseVisualStyleBackColor = true;
@@ -155,9 +155,9 @@ namespace AutoCAD_Block_Counter
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(307, 420);
+            this.btnCancel.Location = new System.Drawing.Point(460, 520);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(65, 30);
+            this.btnCancel.Size = new System.Drawing.Size(100, 35);
             this.btnCancel.TabIndex = 6;
             this.btnCancel.Text = "取消";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -167,8 +167,7 @@ namespace AutoCAD_Block_Counter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 470);
-            this.Controls.Add(this.chkShowLayer);
+            this.ClientSize = new System.Drawing.Size(600, 600);
             this.Controls.Add(this.btnImportBlockMap);
             this.Controls.Add(this.btnAddBlock);
             this.Controls.Add(this.dgvBlocks);
@@ -181,6 +180,7 @@ namespace AutoCAD_Block_Counter
             this.Controls.Add(this.flowSegments);
             this.Controls.Add(this.lblFileName);
             this.Controls.Add(this.lblHint);
+            this.Controls.Add(this.chkShowLayer);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
